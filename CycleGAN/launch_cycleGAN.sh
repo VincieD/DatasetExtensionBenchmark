6965abf9d359cecd2d999d51b5ae98d4351c5ed9
Vaclav Divis <divisvaclav@seznam.cz>
2019-11-13 07:56:08 +0100

    Update launch_cycleGAN.sh

--- CycleGAN/launch_cycleGAN.sh
+++ CycleGAN/launch_cycleGAN.sh
@@ -9,8 +9,8 @@ module add cudnn-7.0
 # activate virtual enviroment
 # on KONOS7
 cd ~/DatasetExtensionBenchmark
-virtualenv ENV_CYCLE_GAN_TF_1_14
-source ENV_CYCLE_GAN_TF_1_14/bin/activate
+virtualenv ENV_CYCLE_GAN_TF_1_12
+source ENV_CYCLE_GAN_TF_1_12/bin/activate
 
 
 python train.py --X=data/tfrecords/summer.tfrecords --Y=data/tfrecords/winter.tfrecords --batch_size=1 --image_size=256
